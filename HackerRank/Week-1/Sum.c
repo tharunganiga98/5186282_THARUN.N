7ce3b657d8241d081d01e786fbdecd8181e94b62
tharunganiga98 <tharunthor05@gmail.com>
2025-08-05 00:53:55 +0530

    code

---
+++ HackerRank/Week-1/Sum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void Twosum(long arr[5] , int n){
+    long sum=0, min =arr[0], max=arr[0];
+    
+    for(int i=0; i<n; i++){
+        sum = sum+arr[i];
+        if(arr[i]<min)
+            min=arr[i];
+        if(arr[i]>max)
+            max=arr[i];
+        
+    }
+    printf("%ld %ld\n", sum-max , sum-min);
+}
+
+int main(){
+    long arr[5];
+    for(int i=0; i<5; i++){
+        scanf("%ld\n", &arr[i]);
+    }
+    Twosum(arr,5);
+    return 0;  
+}
